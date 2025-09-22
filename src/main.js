a6fff6f156c8b1b85b27a931a86134e57b8481b9
Aldo <aldo-panzano@hotmail.it>
2025-09-22 12:50:17 +0200

    add icons

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ async function loadInitialConfig(url) {
 function registerServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('sw.js')
+      navigator.serviceWorker.register('/dietplanner/sw.js')
         .then(registration => console.log('ServiceWorker registration successful'))
         .catch(err => console.log('ServiceWorker registration failed: ', err));
     });
