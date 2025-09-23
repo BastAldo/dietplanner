c3fe8d907e48d1b477d4c735e92e55d14cb07eae
Aldo <aldo-panzano@hotmail.it>
2025-09-23 11:11:13 +0200

    fix(core): Add missing WEEK_STARTS_ON_MONDAY constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const DEFAULT_CONFIG_URL = '';
 export const LOCAL_STORAGE_KEY_PLAN = 'dynamicProtocolPlan';
 export const LOCAL_STORAGE_KEY_URL = 'dynamicConfigUrl';
+export const WEEK_STARTS_ON_MONDAY = 1;
 export const DAYS = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica'];
 export const MEAL_TYPES = [
   'Colazione',
